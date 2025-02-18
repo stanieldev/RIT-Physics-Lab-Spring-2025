dd55a58b850a668b1579339591aff70c3a36319d
Stanley Goodwin <github@staniel.dev>
2025-02-18 11:21:07 -0500

    Update differential_equations.py

--- stanieldev/differential_equations.py
+++ stanieldev/differential_equations.py
@@ -1,67 +1,45 @@
 import numpy as np
+from interpolation import find_two_closest_points, linear_interpolation
 
 
-# Value Summation Functions
-def eulers_method(
+# Euler's Method
+def eulers_method_value(
     f: callable,
     t0: float | int,
     x0: float | int | np.ndarray | tuple | list,
     dt: float | int,
     tf: float | int
 ) -> float:
+    """
+    Euler's Method for solving ordinary differential equations.
+    This function solves the ODE using Euler's Method and returns the final value.
+    
+    Parameters:
+        f: callable - The function equal to the derivative of the unknown function
+        t0: float | int - The initial time
+        x0: float | int - The initial value(s)
+        dt: float | int - The time step
+        tf: float | int - The final time
+        
+    Returns:
+        float - The value of the unknown function at the final time
+    """
+
+    # Input Checking
     assert callable(f)
     assert isinstance(t0, (int, float))
     assert isinstance(x0, (int, float, np.ndarray, tuple, list))
     assert isinstance(dt, (int, float))
     assert isinstance(tf, (int, float))
-    t = np.arange(t0, tf+dt, dt)
+    assert t0 < tf, "t0 must be less than tf"
+    assert tf - t0 > dt, "dt is too large for the given time frame"
+
+    # Implementation
+    T = np.arange(t0, tf, dt)
     x = x0
-    for i in range(len(t)-1):
-        x = x + f(t[i], x)*dt
+    for t in T: x += f(t, x)*dt
     return x
 
-def midpoint_method(
-    f: callable,
-    t0: float | int,
-    x0: float | int | np.ndarray | tuple | list,
-    dt: float | int,
-    tf: float | int
-) -> float:
-    assert callable(f)
-    assert isinstance(t0, (int, float))
-    assert isinstance(x0, (int, float, np.ndarray, tuple, list))
-    assert isinstance(dt, (int, float))
-    assert isinstance(tf, (int, float))
-    t = np.arange(t0, tf+dt, dt)
-    x = x0
-    for i in range(len(t)-1):
-        x = x + f(t[i] + dt/2, x + f(t[i], x)*dt/2)*dt
-    return x
-
-def runge_kutta_method(
-    f: callable,
-    t0: float | int,
-    x0: float | int | np.ndarray | tuple | list,
-    dt: float | int,
-    tf: float | int
-) -> float:
-    assert callable(f)
-    assert isinstance(t0, (int, float))
-    assert isinstance(x0, (int, float, np.ndarray, tuple, list))
-    assert isinstance(dt, (int, float))
-    assert isinstance(tf, (int, float))
-    t = np.arange(t0, tf+dt, dt)
-    x = x0
-    for i in range(len(t)-1):
-        k1 = f(t[i], x)
-        k2 = f(t[i] + dt/2, x + k1*dt/2)
-        k3 = f(t[i] + dt/2, x + k2*dt/2)
-        k4 = f(t[i] + dt, x + k3*dt)
-        x = x + (k1 + 2*k2 + 2*k3 + k4)*dt/6
-    return x
-
-
-# Integrating Functions
 def eulers_method_function(
     f: callable,
     t0: float | int,
@@ -69,17 +47,73 @@ def eulers_method_function(
     dt: float | int,
     tf: float | int
 ) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Euler's Method for solving ordinary differential equations.
+    This function solves the ODE using Euler's Method and returns the time and value arrays.
+    
+    Parameters:
+        f: callable - The function equal to the derivative of the unknown function
+        t0: float | int - The initial time
+        x0: float | int - The initial value(s)
+        dt: float | int - The time step
+        tf: float | int - The final time
+
+    Returns:
+        tuple[np.ndarray, np.ndarray] - The time and value arrays
+    """
+
+    # Input Checking
     assert callable(f)
     assert isinstance(t0, (int, float))
     assert isinstance(x0, (int, float, np.ndarray, tuple, list))
     assert isinstance(dt, (int, float))
     assert isinstance(tf, (int, float))
-    t = np.arange(t0, tf+dt, dt)
-    x = [x0]
-    for i in range(len(t)-1):
-        x.append(x[-1] + f(t[i], x[-1])*dt)
-    return t, x
+    assert t0 < tf, "t0 must be less than tf"
+    assert tf - t0 > dt, "dt is too large for the given time frame"
 
+    # Implementation
+    T = np.arange(t0, tf+dt, dt)
+    x = [x0]
+    for t in T[:-1]: x.append(x[-1] + f(t, x[-1])*dt)
+    return T, x
+
+
+# Midpoint Method
+def midpoint_method_value(
+    f: callable,
+    t0: float | int,
+    x0: float | int | np.ndarray | tuple | list,
+    dt: float | int,
+    tf: float | int
+) -> float:
+    """
+    Midpoint Method for solving ordinary differential equations.
+    This function solves the ODE using the Midpoint Method and returns the final value.
+    
+    Parameters:
+        f: callable - The function equal to the derivative of the unknown function
+        t0: float | int - The initial time
+        x0: float | int - The initial value(s)
+        dt: float | int - The time step
+        tf: float | int - The final time
+    
+    Returns:
+        float - The value of the unknown function at the final time
+    """
+    
+    # Input Checking
+    assert callable(f)
+    assert isinstance(t0, (int, float))
+    assert isinstance(x0, (int, float, np.ndarray, tuple, list))
+    assert isinstance(dt, (int, float))
+    assert isinstance(tf, (int, float))
+
+    # Implementation
+    T = np.arange(t0, tf+dt, dt)
+    x = x0
+    for t in T[:-1]: x += f(t + dt/2, x + f(t, x)*dt/2)*dt
+    return x
+        
 def midpoint_method_function(
     f: callable,
     t0: float | int,
@@ -87,16 +121,75 @@ def midpoint_method_function(
     dt: float | int,
     tf: float | int
 ) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Midpoint Method for solving ordinary differential equations.
+    This function solves the ODE using the Midpoint Method and returns the time and value arrays.
+    
+    Parameters:
+        f: callable - The function equal to the derivative of the unknown function
+        t0: float | int - The initial time
+        x0: float | int - The initial value(s)
+        dt: float | int - The time step
+        tf: float | int - The final time
+
+    Returns:
+        tuple[np.ndarray, np.ndarray] - The time and value arrays
+    """
+
+    # Input Checking
     assert callable(f)
     assert isinstance(t0, (int, float))
     assert isinstance(x0, (int, float, np.ndarray, tuple, list))
     assert isinstance(dt, (int, float))
     assert isinstance(tf, (int, float))
-    t = np.arange(t0, tf+dt, dt)
+
+    # Implementation
+    T = np.arange(t0, tf+dt, dt)
     x = [x0]
-    for i in range(len(t)-1):
-        x.append(x[-1] + f(t[i] + dt/2, x[-1] + f(t[i], x[-1])*dt/2)*dt)
-    return t, x
+    for t in T[:-1]: x.append(x[-1] + f(t + dt/2, x[-1] + f(t, x[-1])*dt/2)*dt)
+    return T, x
+
+
+# Runge-Kutta Method
+def runge_kutta_method_value(
+    f: callable,
+    t0: float | int,
+    x0: float | int | np.ndarray | tuple | list,
+    dt: float | int,
+    tf: float | int
+) -> float:
+    """
+    Runge-Kutta Method for solving ordinary differential equations.
+    This function solves the ODE using the Runge-Kutta Method and returns the final value.
+    
+    Parameters:
+        f: callable - The function equal to the derivative of the unknown function
+        t0: float | int - The initial time
+        x0: float | int - The initial value(s)
+        dt: float | int - The time step
+        tf: float | int - The final time
+    
+    Returns:
+        float - The value of the unknown function at the final time
+    """
+    
+    # Input Checking
+    assert callable(f)
+    assert isinstance(t0, (int, float))
+    assert isinstance(x0, (int, float, np.ndarray, tuple, list))
+    assert isinstance(dt, (int, float))
+    assert isinstance(tf, (int, float))
+
+    # Implementation
+    T = np.arange(t0, tf+dt, dt)
+    x = x0
+    for t in T[:-1]:
+        k1 = f(t       , x          )
+        k2 = f(t + dt/2, x + k1*dt/2)
+        k3 = f(t + dt/2, x + k2*dt/2)
+        k4 = f(t + dt  , x + k3*dt  )
+        x += (k1 + 2*k2 + 2*k3 + k4)*dt/6
+    return x
 
 def runge_kutta_method_function(
     f: callable,
@@ -105,17 +198,35 @@ def runge_kutta_method_function(
     dt: float | int,
     tf: float | int
 ) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Runge-Kutta Method for solving ordinary differential equations.
+    This function solves the ODE using the Runge-Kutta Method and returns the time and value arrays.
+    
+    Parameters:
+        f: callable - The function equal to the derivative of the unknown function
+        t0: float | int - The initial time
+        x0: float | int - The initial value(s)
+        dt: float | int - The time step
+        tf: float | int - The final time
+
+    Returns:
+        tuple[np.ndarray, np.ndarray] - The time and value arrays
+    """
+
+    # Input Checking
     assert callable(f)
     assert isinstance(t0, (int, float))
     assert isinstance(x0, (int, float, np.ndarray, tuple, list))
     assert isinstance(dt, (int, float))
     assert isinstance(tf, (int, float))
-    t = np.arange(t0, tf+dt, dt)
+
+    # Implementation
+    T = np.arange(t0, tf+dt, dt)
     x = [x0]
-    for i in range(len(t)-1):
-        k1 = f(t[i], x[-1])
-        k2 = f(t[i] + dt/2, x[-1] + k1*dt/2)
-        k3 = f(t[i] + dt/2, x[-1] + k2*dt/2)
-        k4 = f(t[i] + dt, x[-1] + k3*dt)
+    for t in T[:-1]:
+        k1 = f(t       , x[-1]          )
+        k2 = f(t + dt/2, x[-1] + k1*dt/2)
+        k3 = f(t + dt/2, x[-1] + k2*dt/2)
+        k4 = f(t + dt  , x[-1] + k3*dt  )
         x.append(x[-1] + (k1 + 2*k2 + 2*k3 + k4)*dt/6)
-    return t, x
+    return T, x
